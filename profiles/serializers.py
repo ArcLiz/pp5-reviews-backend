3527769b77e2f5ece0775282fe0a0a13b02fddf4
ArcLiz <gyllingstenlizah@gmail.com>
2024-06-01 11:47:54 +0200

    Add image size validation for profile picture

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,6 +6,13 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                "Please choose an image smaller than 2MB"
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
