a37b9dc1457cfd746092f1c5dbd9de40558b2041
ArcLiz <gyllingstenlizah@gmail.com>
2024-06-02 20:04:52 +0200

    Fix env files

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ['CLOUDINARY_URL'] = 'cloudinary://979433993888224:cSx4uUljpYcI0cdMuBEZOMP3Ixc@dkkm0av7p'
-os.environ['DEV'] = '1'
-os.environ['ALLOWED_HOST'] = '127.0.0.1'
-os.environ['LOCAL_ALLOWED_HOST'] = 'localhost'
-os.environ['DATABASE_URL'] = 'postgres://tlllwlts:jlF8CyGx2lqjszYaqmH5viEWCKIpG06A@cornelius.db.elephantsql.com/tlllwlts'
-os.environ.setdefault('SECRET_KEY', 'wizzypizzyp00d00')
