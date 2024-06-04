6a61227600632e66626e0ef0f6657e868406ddab
ArcLiz <gyllingstenlizah@gmail.com>
2024-06-04 21:44:49 +0200

    Add new database url

--- main/settings.py
+++ main/settings.py
@@ -147,17 +147,9 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DEV' in os.environ:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-# else:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#     }
 
 
 # Password validation
