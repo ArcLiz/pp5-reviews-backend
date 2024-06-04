bad249e2bbe34b2d9fc03fa168490fd29f8e062e
ArcLiz <gyllingstenlizah@gmail.com>
2024-06-04 23:54:06 +0200

    Add follow/id POST/DELETE view

--- followers/urls.py
+++ followers/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from followers import views
+from .views import follow_user
 
 urlpatterns = [
     path('followers/', views.FollowerList.as_view()),
-    path('followers/<int:pk>/', views.FollowerDetail.as_view())
+    path('followers/<int:pk>/', views.FollowerDetail.as_view()),
+    path('follow/<int:followed_id>/', follow_user, name='follow_user'),
 ]
--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,6 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import generics, permissions
 from main.permissions import IsOwnerOrReadOnly
 from .models import Follower
@@ -28,3 +31,36 @@ class FollowerDetail(generics.RetrieveDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Follower.objects.all()
     serializer_class = FollowerSerializer
+
+
+@api_view(['POST', 'DELETE'])
+def follow_user(request, followed_id):
+    if not request.user.is_authenticated:
+        return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    if request.method == 'POST':
+        # Check if user already follows the specified user
+        existing_followers = Follower.objects.filter(
+            owner=request.user, followed=followed_id)
+        if existing_followers.exists():
+            return Response({'error': 'User already followed'}, status=status.HTTP_200_OK)
+
+        # Create a new follower instance
+        follower_data = {'owner': request.user.id, 'followed': followed_id}
+        serializer = FollowerSerializer(data=follower_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        # Check if the follower exists
+        try:
+            follower_to_delete = Follower.objects.get(
+                owner=request.user, followed=followed_id)
+        except Follower.DoesNotExist:
+            return Response({'error': 'Follower does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+        # Remove the follow
+        follower_to_delete.delete()
+        return Response({'message': 'Unfollowed user'}, status=status.HTTP_200_OK)
