acdde527290e4239744c23efd5e0d7379e11242c
ArcLiz <gyllingstenlizah@gmail.com>
2024-06-27 15:32:39 +0200

    Fix create new review

--- reviews/views.py
+++ reviews/views.py
@@ -1,21 +1,18 @@
-from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from django.http import Http404
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Review
 from .serializers import ReviewSerializer
 from main.permissions import IsAuthorOrReadOnly
-from rest_framework import filters
 
 
-class ReviewList(ListAPIView):
+class ReviewList(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated]
-    filter_backends = [filters.OrderingFilter,
-                       filters.SearchFilter, DjangoFilterBackend]
+    permission_classes = [IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
     filterset_fields = ['owner', 'book']
 
 
