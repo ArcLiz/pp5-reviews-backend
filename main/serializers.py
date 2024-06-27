74568e993ed701a9779f3bf786d25cff884794c9
ArcLiz <gyllingstenlizah@gmail.com>
2024-06-27 21:27:16 +0200

    Add admin status to CurrentUser Serializer

--- main/serializers.py
+++ main/serializers.py
@@ -5,8 +5,12 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    is_admin = serializers.SerializerMethodField()
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'profile_id', 'profile_image', 'is_admin'
         )
+
+    def get_is_admin(self, obj):
+        return obj.is_staff
