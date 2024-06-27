5eee4f336927a5e43a1d3404e4486e6cd1ee7875
Lizah Maria <120017239+ArcLiz@users.noreply.github.com>
2024-06-27 12:24:10 +0200

    Revert changes to CurrentUserSerializer due to unknown 500 error

--- main/serializers.py
+++ main/serializers.py
@@ -5,9 +5,8 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
-    is_superuser = serializers.ReadOnlyField(source='is_superuser')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image', 'is_superuser'
+            'profile_id', 'profile_image'
         )
