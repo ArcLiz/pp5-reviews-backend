2e1c6c684f1b168df0c4e79e30520e19dc23f189
ArcLiz <gyllingstenlizah@gmail.com>
2024-07-01 16:56:13 +0200

    Add list views for series and genres, fix authenticated user permissions to create book

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from books import views
-from .views import BookList, BookDetails, BookCreate
+from .views import BookList, BookDetails, BookCreate, GenreListCreate, SeriesListCreate, SeriesDetails
 
 urlpatterns = [
-    path('books/', views.BookList.as_view()),
-    path('books/<int:pk>/', views.BookDetails.as_view()),
-    path('books/create/', views.BookCreate.as_view())
+    path('books/', BookList.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetails.as_view(), name='book-details'),
+    path('books/create/', BookCreate.as_view(), name='book-create'),
+    path('genres/', GenreListCreate.as_view(), name='genre-list-create'),
+    path('series/', SeriesListCreate.as_view(), name='series-list-create'),
+    path('series/<int:pk>/', SeriesDetails.as_view(), name='series-list-create'),
 ]
--- books/views.py
+++ books/views.py
@@ -2,8 +2,8 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book, Genre, Series
+from .serializers import BookSerializer, GenreSerializer, SeriesSerializer
 from main.permissions import IsAdminOrReadOnly
 from django.http import Http404
 
@@ -29,9 +29,9 @@ class BookDetails(APIView):
 
     def get(self, request, pk):
         try:
-            book = Book.objects.get(pk=pk)
-        except Book.DoesNotExist:
-            return Response(status=404)
+            book = self.get_object(pk)
+        except Http404:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = BookSerializer(book)
         return Response(serializer.data)
 
@@ -44,15 +44,66 @@ class BookDetails(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        try:
-            book = Book.objects.get(pk=pk)
-        except Book.DoesNotExist:
-            return Response(status=404)
+        book = self.get_object(pk)
         book.delete()
-        return Response(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class BookCreate(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        genres_data = self.request.data.pop('genres', [])
+        new_genres_data = self.request.data.pop('new_genres', [])
+
+        series_data = self.request.data.pop('series', None)
+        new_series_name = self.request.data.pop('new_series_name', None)
+
+        if new_series_name:
+            series, created = Series.objects.get_or_create(
+                name=new_series_name)
+        elif series_data:
+            series, created = Series.objects.get_or_create(**series_data)
+        else:
+            series = None
+
+        book = serializer.save(series=series, **self.request.data)
+
+        for genre_id in genres_data:
+            genre = Genre.objects.get(id=genre_id)
+            book.genres.add(genre)
+
+        for genre_name in new_genres_data:
+            genre, created = Genre.objects.get_or_create(name=genre_name)
+            book.genres.add(genre)
+
+        return book
+
+
+class GenreListCreate(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly & IsAdminOrReadOnly]
+
+
+class SeriesListCreate(generics.ListCreateAPIView):
+    queryset = Series.objects.all()
+    serializer_class = SeriesSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly & IsAdminOrReadOnly]
+
+
+class SeriesDetails(APIView):
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request, pk):
+        try:
+            series = Series.objects.get(pk=pk)
+        except Series.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        books = Book.objects.filter(series=series)
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
